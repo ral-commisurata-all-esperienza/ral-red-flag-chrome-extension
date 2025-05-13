2a848a35043985d5c3f8c6812e69be6809cb5205
Alessio Izzo <alessio.izzo86@gmail.com>
2025-05-13 15:14:29 +0200

    merge

--- content.js
+++ content.js
@@ -60,3 +60,22 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     f();
   }
 });
+const notifyDescriptionLoaded = () => {
+  console.log("DOM content loaded in the tab");
+  const intervalId = setInterval(() => {
+    const jobDescription = document.getElementById("job-details")?.innerText;
+    if (jobDescription !== null && jobDescription !== "About the job") {
+      chrome.runtime.sendMessage({ message: "domContentLoaded" });
+      clearInterval(intervalId);
+      return;
+    }
+  }, 1000);
+};
+
+if (document.readyState === "loading") {
+  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", notifyDescriptionLoaded);
+} else {
+  // `DOMContentLoaded` has already fired
+  notifyDescriptionLoaded();
+}
